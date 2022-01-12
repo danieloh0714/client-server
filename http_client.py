e23d1ce78ea0f012be9ddca62a9198ef1035ac6c
Danny Oh <dannyoh0316@gmail.com>
2022-01-11 22:19:07 -0600

    handle limited number of redirects

--- http_client.py
+++ http_client.py
@@ -3,6 +3,9 @@ from socket import socket, AF_INET, SOCK_STREAM
 from sys import argv, exit, stderr, stdout
 
 
+REDIRECT_LIMIT = 10
+
+
 def get_url_input() -> str:
     # program should take exactly one parameter
     if len(argv) != 2:
@@ -37,60 +40,70 @@ def get_header_info(s, data: list) -> str:
     return "".join(ans)
 
 
-def make_get_request(url: str) -> bool:
-    host, page, port = get_url_parts(url)
-
-    s = socket(AF_INET, SOCK_STREAM)
-    s.connect((host, port))
-    req = f"GET {page} HTTP/1.0\r\nHost: {host}\r\n\r\n"
-    s.sendall(req.encode())
-
-    status_code = 0
-    content_len = 0
-    content_type = ""
-
+def make_get_request(input_url: str) -> bool:
+    url = input_url
     data = []
-    is_body = False
-    while True:
-        # receive one byte at a time; break when no more
-        buf = s.recv(1)
-        if not buf:
+    status_code = 0
+    for _ in range(REDIRECT_LIMIT + 1):
+        if data:
             break
-        data.append(buf.decode())
 
-        # get response status code
-        if len(data) == 12:
-            status_code = int("".join(data[-3:]))
+        host, page, port = get_url_parts(url)
 
-        # if two consecutive returns, then body starts
-        if "".join(data[-4:]) == "\r\n\r\n":
-            is_body = True
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((host, port))
+        req = f"GET {page} HTTP/1.0\r\nHost: {host}\r\n\r\n"
+        s.sendall(req.encode())
 
-        # if body started and content length specified, break when content length bytes are received
-        if is_body and content_len:
-            content_len -= 1
-            if content_len == 0:
+        content_len = 0
+        content_type = ""
+        is_body = False
+        while True:
+            # receive one byte at a time; break when no more
+            buf = s.recv(1)
+            if not buf:
                 break
+            data.append(buf.decode())
 
-        # if body not started, check for content length and content type info
-        if not is_body:
-            if not content_len and "".join(data[-16:]) == "Content-Length: ":
-                content_len = int(get_header_info(s, data))
-            if not content_type and "".join(data[-14:]) == "Content-Type: ":
-                content_type = get_header_info(s, data)
-                if content_type[:9] != "text/html":
-                    return False
-            if status_code in [301, 302] and "".join(data[-10:]) == "Location: ":
-                redirect_url = get_header_info(s, data)
-                return make_get_request(redirect_url)
+            # get response status code
+            if len(data) == 12:
+                status_code = int("".join(data[-3:]))
+
+            # if two consecutive returns, then body starts
+            if "".join(data[-4:]) == "\r\n\r\n":
+                is_body = True
+
+            # if body started and content length specified, break when content length bytes are received
+            if is_body and content_len:
+                content_len -= 1
+                if content_len == 0:
+                    break
+
+            # if body not started, check for header info
+            if not is_body:
+                if not content_len and "".join(data[-16:]) == "Content-Length: ":
+                    content_len = int(get_header_info(s, data))
+                if not content_type and "".join(data[-14:]) == "Content-Type: ":
+                    content_type = get_header_info(s, data)
+                    if content_type[:9] != "text/html":
+                        return False
+                if status_code in [301, 302] and "".join(data[-10:]) == "Location: ":
+                    url = get_header_info(s, data)
+                    data = []
+                    status_code = 0
+                    break
+
+    # redirect limit reached
+    if not data:
+        return False
 
     stdout.write("".join(data))
     return status_code < 400
 
 
 def main() -> None:
-    url = get_url_input()
-    if not make_get_request(url):
+    input_url = get_url_input()
+    if not make_get_request(input_url):
         exit(1)
     exit(0)
 
